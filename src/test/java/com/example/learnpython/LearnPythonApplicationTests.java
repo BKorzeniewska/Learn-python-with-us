f406115a7063fc26a941e740d9b4c50873e9bdb6
Michauuek <michal.rzemieniuk@gmail.com>
2023-03-25 10:59:25 +0100

    Initial commit

---
+++ src/test/java/com/example/learnpython/LearnPythonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnpython;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnPythonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
