1e88acc9d0836142f80f63f4a74d6219ed8b3b54
Lord225 <zlotymaciej@gmail.com>
2023-04-04 14:40:17 +0200

    u paying me too little

--- client/src/components/home/HomeScreen.tsx
+++ client/src/components/home/HomeScreen.tsx
@@ -1,38 +1,40 @@
 import React, { useContext } from 'react';
-import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { Container, Navbar, Nav, Button, Row } from 'react-bootstrap';
 import { MainNavbar } from '../common/layout/MainNavbar';
 import { ThemeContext } from '../themes/ThemeProvider';
 import '../../App.css';
 
 type Props = {
-    
+
 }
 
-const HomeScreen = (props : Props) =>{
-    const { theme, toggleTheme } = useContext(ThemeContext);
+const HomeScreen = (props: Props) => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
-    return (
-        <>
-          <MainNavbar/>    
-          <Container className="mt-3" data-theme={theme}>
-            <Button
-              onClick={() => {
-                toggleTheme();
-              }}
-            >
-              {theme === 'light' ? 'Dark' : 'Light'} Theme
-            </Button>
+  return (
+    <>
+      <MainNavbar />
+      <div id="App" data-theme={theme}>
+        <Container className="pt-3">
+          <Button
+            onClick={() => {
+              toggleTheme();
+            }}
+          >
+            {theme === 'light' ? 'Dark' : 'Light'} Theme
+          </Button>
 
-            <Button
-              onClick={() => {
-                toggleTheme();
-              }}
-              variant="secondary"
-            >
-              Secondary button
-            </Button>
-          </Container>
-        </>
-      );
+          <Button
+            onClick={() => {
+              toggleTheme();
+            }}
+            variant="secondary"
+          >
+            Secondary button
+          </Button>
+        </Container>
+      </div>
+    </>
+  );
 }
 export default HomeScreen
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { ThemeProvider } from './components/themes/ThemeProvider';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ThemeProvider>
