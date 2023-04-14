6e82a215dfef88e55e765f8a357fd976f5fb97a8
Michauuek <michal.rzemieniuk@gmail.com>
2023-04-14 19:34:55 +0200

    Add email sending support

---
+++ src/main/java/com/example/learnpython/mail/EmailSenderService.java
@@ -0,0 +1,61 @@
+package com.example.learnpython.mail;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import java.io.File;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailSenderService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendSimpleEmail(String toEmail,
+                                String body,
+                                String subject) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom("nasz.teatr.krakow@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+        log.info("Mail Send to {}", toEmail);
+    }
+
+    public void sendEmailWithAttachment(String toEmail,
+                                        String body,
+                                        String subject,
+                                        String attachment) throws MessagingException {
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper
+                = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom("nasz.teatr.krakow@gmail.com");
+        mimeMessageHelper.setTo(toEmail);
+        mimeMessageHelper.setText(body);
+        mimeMessageHelper.setSubject(subject);
+
+        FileSystemResource fileSystem
+                = new FileSystemResource(new File(attachment));
+
+        mimeMessageHelper.addAttachment(fileSystem.getFilename(),
+                fileSystem);
+
+        mailSender.send(mimeMessage);
+        log.info("Mail Send to {}", toEmail);
+
+    }
+}
