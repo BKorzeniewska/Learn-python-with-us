fa8c18578b1e8aae4c60f4e1d8f13f0a93edec50
BKorzeniewska <b.a.korzeniewska@wp.pl>
2023-04-20 01:00:11 +0200

    for repair

--- src/main/java/com/example/learnpython/challenge/ChallengeController.java
+++ src/main/java/com/example/learnpython/challenge/ChallengeController.java
@@ -1,12 +1,23 @@
 package com.example.learnpython.challenge;
 
+import com.example.learnpython.chapter.ChapterResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/challenge")
 @RequiredArgsConstructor
 public class ChallengeController {
     private final ChallengeService challengeService;
+    @GetMapping
+    public ResponseEntity<List<ChapterResponse>> getChallanges() {
+        var challenges = challengeService.getChallenges();
+        return new ResponseEntity<>(challenges, HttpStatus.OK);
+    }
 }
