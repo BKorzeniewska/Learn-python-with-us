6180464b21ceec99d67b187fc756d8509e1450ae
Batat <b.a.korzeniewska@wp.pl>
2023-04-20 12:23:43 +0200

    fix

--- src/main/java/com/example/learnpython/challenge/ChallengeController.java
+++ src/main/java/com/example/learnpython/challenge/ChallengeController.java
@@ -1,12 +1,12 @@
 package com.example.learnpython.challenge;
 
+import com.example.learnpython.article.ArticleResponse;
 import com.example.learnpython.chapter.ChapterResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +16,20 @@ import java.util.List;
 public class ChallengeController {
     private final ChallengeService challengeService;
     @GetMapping
-    public ResponseEntity<List<ChapterResponse>> getChallanges() {
+    public ResponseEntity<List<ChallengeResponse>> getChallenges() {
         var challenges = challengeService.getChallenges();
         return new ResponseEntity<>(challenges, HttpStatus.OK);
     }
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<ChallengeResponse> getChallengeById(@PathVariable("id") Long challengeId) {
+        var challenge = challengeService.getChallengeById(challengeId);
+        return new ResponseEntity<>(challenge, HttpStatus.OK);
+    }
+    @GetMapping("/name/{nameFragment}")
+    public ResponseEntity<List<ChallengeResponse>> getChallengeByNameContaining(@PathVariable("nameFragment") String nameFragment) {
+        var challenge = challengeService.getChallengeByName(nameFragment);
+        return new ResponseEntity<>(challenge, HttpStatus.OK);
+    }
+
 }
