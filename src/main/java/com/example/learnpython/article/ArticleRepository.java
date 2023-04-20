3424642784d590033508e17f11d34fc62c905fde
BKorzeniewska <b.a.korzeniewska@wp.pl>
2023-04-20 21:33:10 +0200

    add query annotation

--- src/main/java/com/example/learnpython/article/ArticleRepository.java
+++ src/main/java/com/example/learnpython/article/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.example.learnpython.article;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,8 +12,12 @@ import java.util.Optional;
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     Optional<List<Article>> findByChapterId(Long chapterId);
+
+    @Query("SELECT a FROM Article a WHERE a.title LIKE %:titleFragment%")
     Optional<List<Article>> findByTitleContaining(String titleFragment);
+    @Query("SELECT a FROM Article a WHERE a.creationDate BETWEEN :startDate AND :endDate")
     Optional<List<Article>> findByCreationDateBetween(LocalDate startDate, LocalDate endDate);
+    @Query("SELECT a FROM Article a WHERE a.creationDate=:date")
     Optional<List<Article>> findByCreationDate(LocalDate date);
 
 }
