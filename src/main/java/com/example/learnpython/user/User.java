594b5207ab0ff34e0ea234c261192b8f47d23ef9
Batat <b.a.korzeniewska@wp.pl>
2023-04-21 10:52:09 +0200

    add level & exp to user

--- src/main/java/com/example/learnpython/user/User.java
+++ src/main/java/com/example/learnpython/user/User.java
@@ -36,6 +36,12 @@ public class User implements UserDetails {
     private String nickname;
     @Column(name = "PASSWORD")
     private String password;
+
+    @Column(name = "LEVEL")
+    private Integer level;
+
+    @Column(name = "EXP")
+    private Long exp;
     @Enumerated(EnumType.STRING)
     private Role role;
     @OneToMany(mappedBy = "user")
