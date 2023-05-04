de6b38e66216cc692923f342e5a2af33dff73519
Batat <b.a.korzeniewska@wp.pl>
2023-05-04 12:52:15 +0200

    add get challenges by article id

--- src/main/java/com/example/learnpython/challenge/model/ContentJson.java
+++ src/main/java/com/example/learnpython/challenge/model/ContentJson.java
@@ -4,9 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContentJson {
     protected String correctAnswer;
+    protected Type typeQuestion;
+    protected List< String> answers;
 }
