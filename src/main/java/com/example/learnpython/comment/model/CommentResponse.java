e98507c518692a073c9b36ca795ad9ea1a92ec6a
Michauuek <michal.rzemieniuk@gmail.com>
2023-05-19 17:55:04 +0200

    Fix comments reposnse

--- src/main/java/com/example/learnpython/comment/model/CommentResponse.java
+++ src/main/java/com/example/learnpython/comment/model/CommentResponse.java
@@ -12,5 +12,15 @@ public class CommentResponse {
     private String content;
     private LocalDateTime createdAt;
     private Long articleId;
-    private Long userId;
+    private UserDetails userDetails;
+
+    @Data
+    @Builder
+    public static class UserDetails {
+        private Long id;
+        private String nickname;
+        private String email;
+        private String firstname;
+    }
+
 }
