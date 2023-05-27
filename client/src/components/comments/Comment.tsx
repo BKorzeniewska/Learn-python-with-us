4075e44b9ddcbedc7a56e19aedb3b08a6610e6bf
michal <minauka@tuta.io>
2023-05-27 15:54:05 +0200

    comments improved

--- client/src/components/comments/Comment.tsx
+++ client/src/components/comments/Comment.tsx
@@ -3,23 +3,35 @@ import { CommentResponse } from "./apis/comment";
 import { UserInfo, loadUserById } from "../user/apis/profile";
 import { useError } from "../home/ErrorContext";
 import { Card } from "react-bootstrap";
+import { generateRandomPixels } from "../user/avatarGen";
+import "./comments.css";
 
 type Props = {
   data: CommentResponse;
 };
 
 export const Comment = (props: Props) => {
+  const formattedDate = new Date(props.data.createdAt).toLocaleString();
+
   return (
     <Card className="mb-3">
       <Card.Body>
-        <div>
-          <Card.Title>{props.data.userDetails.nickname}</Card.Title>
-          <Card.Text>{props.data.content}</Card.Text>
-          <Card.Text>
-            <small className="text-muted">{props.data.createdAt}</small>
-          </Card.Text>
+        <div className="d-flex align-items-center">
+          <img
+            src={`data:image/png;base64,${generateRandomPixels(props.data.userDetails.nickname + props.data.userDetails.email)}`}
+            className="rounded-circle avatar"
+            width={25}
+          />
+          <div>
+            <Card.Title>{props.data.userDetails.nickname}</Card.Title>
+            <Card.Text>{props.data.content}</Card.Text>
+            <Card.Text>
+              <small className="text-muted">{formattedDate}</small>
+            </Card.Text>
+          </div>
         </div>
       </Card.Body>
     </Card>
   );
 };
+
