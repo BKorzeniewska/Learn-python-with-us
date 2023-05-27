d2b8a318f4a7680d21a6fc232f366eb665833a0b
Michauuek <michal.rzemieniuk@gmail.com>
2023-05-27 08:35:40 +0200

    Update error handling

--- src/main/java/com/example/learnpython/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/learnpython/exception/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.learnpython.exception;
 
 import com.example.learnpython.token.ExpiredTokenException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,11 +10,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.time.LocalDateTime;
 
+@Log4j2
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BaseServiceException.class)
     public ResponseEntity<ErrorResponse> handleProductServiceException(BaseServiceException exception) {
+
+        log.error("BaseServiceException error: ", exception);
+        log.error("BaseServiceException error: {}", exception.getMessage());
+        log.error("Unexpected error code: {}", exception.getErrorCode());
+
         return new ResponseEntity<>(new ErrorResponse().builder()
                 .errorMessage(exception.getMessage())
                 .errorCode(exception.getErrorCode())
@@ -25,6 +32,11 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception exception) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        log.error("Unexpected error: ", exception);
+        log.error("Unexpected error message: {}", exception.getMessage());
+        log.error("Unexpected error localized message: {}", exception.getLocalizedMessage());
+
         ErrorResponse errorResponse = new ErrorResponse(
                 LocalDateTime.now(),
                 "UNEXPECTED_ERROR",
