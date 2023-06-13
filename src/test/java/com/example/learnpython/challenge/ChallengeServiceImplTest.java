19a25feae3e088957a96ed51a3c0a905e3edfdda
BKorzeniewska <b.a.korzeniewska@wp.pl>
2023-06-13 14:48:37 +0200

    add test

--- src/test/java/com/example/learnpython/challenge/ChallengeServiceImplTest.java
+++ src/test/java/com/example/learnpython/challenge/ChallengeServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.example.learnpython.challenge;
 
 import com.example.learnpython.article.repository.ArticleRepository;
+import com.example.learnpython.challenge.exception.ChallengeNotFoundException;
+import com.example.learnpython.challenge.model.Type;
 import com.example.learnpython.challenge.repository.ChallengeRepository;
 import com.example.learnpython.challenge.service.ChallengeService;
 import com.example.learnpython.challenge.service.ChallengeServiceImpl;
@@ -10,15 +12,17 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ChallengeServiceImplTest {
 
     @Mock
-    private ChallengeRepository challengeRepository;
+    private ChallengeRepository mockchallengeRepository;
     @Mock
     private ChallengeMapper challengeMapper;
     @Mock
@@ -26,14 +30,44 @@ class ChallengeServiceImplTest {
     @Autowired
     private JsonConverter jsonConverter;
 
-    private ChallengeService challengeService;
+    private ChallengeService challengeServiceUnderTest;
     @BeforeEach
     void setUp() {
-        challengeService = new ChallengeServiceImpl(challengeRepository,articleRepository, challengeMapper, jsonConverter);
+        challengeServiceUnderTest = new ChallengeServiceImpl(mockchallengeRepository,articleRepository, challengeMapper, jsonConverter);
     }
 
     @Test
-    public void executePythonCodeTest() {
+    public void findByIdTest() {
+        final Challenge challenge = Challenge.builder()
+                .id(1L)
+                .name("name")
+                .content(jsonConverter.convertToEntityAttribute(""))
+                .question("question")
+                .type(Type.OPEN)
+                .visible(false)
+                .exp(100L)
+                .build();
+        when(mockchallengeRepository.findById(1L)).thenReturn(Optional.of(challenge));
+
+   }
+   /*
+    @Test
+    public void getChallengeByNameTest() {
+        final Challenge challenge = Challenge.builder()
+                .id(1L)
+                .name("name")
+                .content(jsonConverter.convertToEntityAttribute(""))
+                .question("question")
+                .type(Type.OPEN)
+                .visible(false)
+                .exp(100L)
+                .build();
+        final List<Challenge> challengeTest = mockchallengeRepository.findByNameContaining("n").get();
+        assertThat(challengeTest.get(0)).isEqualTo(challenge);
+
+    }
+    @Test
+    public void getChallengesByArticleIdTest() {
 
         when(challengeRepository.findById(1L)).thenReturn(Optional.of(Challenge.builder()
                 .id(1L)
@@ -41,4 +75,33 @@ class ChallengeServiceImplTest {
                 .build()));
 
     }
+    @Test
+    public void executePythonCodeChallengeTest() {
+
+        when(challengeRepository.findById(1L)).thenReturn(Optional.of(Challenge.builder()
+                .id(1L)
+                .name("Challenge 1")
+                .build()));
+
+    }
+
+    @Test
+    public void executeOpenChallengeTest() {
+
+        when(challengeRepository.findById(1L)).thenReturn(Optional.of(Challenge.builder()
+                .id(1L)
+                .name("Challenge 1")
+                .build()));
+
+    }
+    @Test
+    public void executeCloseChallengeTest() {
+
+        when(challengeRepository.findById(1L)).thenReturn(Optional.of(Challenge.builder()
+                .id(1L)
+                .name("Challenge 1")
+                .build()));
+
+    }
+*/
 }
